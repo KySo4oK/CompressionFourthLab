cd39f36cd993da47465fe24845d7d8d20942c219
Rostyslav <prostorostuk08@gmail.com>
2019-05-15 01:29:23 +0300

    Added running program from cmd, 2 or many files to compress

--- src/Compression.java
+++ src/Compression.java
@@ -1,48 +1,65 @@
 import java.io.*;
-import java.util.ArrayList;
 
 public class Compression {
-    Compression(){}
 
-    void compressRlE(String nameOfInputFile, String nameOfOutputFile) throws IOException{
-        try(DataInputStream dataInputStream = new DataInputStream(new FileInputStream(nameOfInputFile));
-            DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(nameOfOutputFile))){
-            byte[] allBytes = dataInputStream.readAllBytes();
-            for(int i = 0; i < allBytes.length; ){
-                int start = i;
-                byte current = allBytes[i];
-                while (i < allBytes.length && allBytes[i]==current){
-                    i++;
+    void compressRlE(String[] args) throws IOException{
+
+        try (DataOutputStream dataOutputStream =
+                        new DataOutputStream(new FileOutputStream(args[0]))){
+            dataOutputStream.write(args.length-1);
+            for(int c = 1; c < args.length ; c++){
+                try (DataInputStream dataInputStream = new DataInputStream(new FileInputStream(args[c]))) {
+                    byte[] allBytes = dataInputStream.readAllBytes();
+                    dataOutputStream.write(args[c].length());
+                    int k;
+                    for (k = 0; k < args[c].length(); k++) {
+                        dataOutputStream.write(args[c].charAt(k));
+                    }
+                    for (int i = 0; i < allBytes.length; ) {
+                        int start = i;
+                        byte current = allBytes[i];
+                        while (i < allBytes.length && allBytes[i] == current && i - start < 256) {
+                            i++;
+                        }
+                        dataOutputStream.write((byte) (i - start));
+                        dataOutputStream.write(current);
+                    }
                 }
-                dataOutputStream.write((byte)(i - start));
-                dataOutputStream.write(current);
+                for(int r = 0; r<3; r++){
+                dataOutputStream.write(0);
+                }
+            }
+        }
+
+    }
+
+    void decompressRLE(String nameOfInputFile) throws IOException{
+        try(DataInputStream dataInputStream = new DataInputStream(new FileInputStream(nameOfInputFile))){
+            byte[] allBytes = dataInputStream.readAllBytes();
+            int quantityOfFile = allBytes[0];
+            for(int z = 1; z < quantityOfFile; z++) {
+                int lengthNameOfFile = allBytes[z];
+                System.out.println(lengthNameOfFile);
+                String nameOfDecompress = "";
+                int k;
+                for (k = 1; k <= lengthNameOfFile; k++) {
+                    char ch = (char) allBytes[k];
+                    nameOfDecompress += Character.toString(ch);
+                }
+                System.out.println(nameOfDecompress);
+                try (DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(nameOfDecompress))) {
+                    for (int i = k; i < allBytes.length - 1; i += 2) {
+                        if(k+2<allBytes.length && allBytes[k] == 0 && allBytes[k+1]==0 && allBytes[k+2]==0){
+                            z = k+2;
+                            break;
+                        }
+                        int value = allBytes[i];
+                        for (int j = 0; j < value; j++) {
+                            dataOutputStream.write(allBytes[i + 1]);
+                        }
+                    }
+                }
             }
         }
     }
-
-    void decompress(String nameOfInputFile){
-        try(DataInputStream dataIn = new DataInputStream(new FileInputStream(nameOfInputFile))){
-
-        } catch (Exception e) {
-            System.out.println("Error input - output");
-        }
-    }
-
-
-    String readFile(String nameOfInputFile){
-        String result = "";
-        int i = 0;
-        try(DataInputStream dataIn = new DataInputStream(new FileInputStream(nameOfInputFile)) ){
-            while (i != -1){
-                i = dataIn.read();
-                result += i;
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        } catch (IOException e) {
-            System.out.println("Error input-output");
-        }
-        return result;
-    }
-
 }
--- src/Start.java
+++ src/Start.java
@@ -2,13 +2,12 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Start {
-
-    public static String nameOfInputFile = "if.bmp";
-    public static String nameOfOutputFile = "of.bmp";
-
     public static void main(String args[]) throws IOException {
-       Compression compression1 = new Compression();
-       compression1.compressRlE(nameOfInputFile,nameOfOutputFile);
+        Compression compression1 = new Compression();
+        if(args.length>1){
+            compression1.decompressRLE(args[0]);
+        } else {
+            compression1.compressRlE(args);
+        }
     }
-
 }
