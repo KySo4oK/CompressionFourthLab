163f91369001a85f393e0e9e4c6d778f1ce53c44
Rostyslav <prostorostuk08@gmail.com>
2019-05-15 03:07:59 +0300

    Added running program from cmd, 2 or many files to compress, try solve problem with length of few files

--- src/Compression.java
+++ src/Compression.java
@@ -6,7 +6,7 @@ public class Compression {
 
         try (DataOutputStream dataOutputStream =
                         new DataOutputStream(new FileOutputStream(args[0]))){
-            dataOutputStream.write(args.length-1);
+            dataOutputStream.writeInt(args.length-1);
             for(int c = 1; c < args.length ; c++){
                 try (DataInputStream dataInputStream = new DataInputStream(new FileInputStream(args[c]))) {
                     byte[] allBytes = dataInputStream.readAllBytes();
@@ -15,18 +15,22 @@ public class Compression {
                     for (k = 0; k < args[c].length(); k++) {
                         dataOutputStream.write(args[c].charAt(k));
                     }
+                    byte[] compressBytes = new byte[allBytes.length];
+                    int point = 0;
                     for (int i = 0; i < allBytes.length; ) {
                         int start = i;
                         byte current = allBytes[i];
                         while (i < allBytes.length && allBytes[i] == current && i - start < 256) {
                             i++;
                         }
-                        dataOutputStream.write((byte) (i - start));
-                        dataOutputStream.write(current);
+                        compressBytes[point] = (byte)(i - start);
+                        point++;
+                        compressBytes[point] = current;
+                        point++;
                     }
-                }
-                for(int r = 0; r<3; r++){
-                dataOutputStream.write(0);
+                    dataOutputStream.writeInt(point);
+                    compressBytes = byteArray(compressBytes, point);
+                    dataOutputStream.write(compressBytes);
                 }
             }
         }
@@ -35,31 +39,35 @@ public class Compression {
 
     void decompressRLE(String nameOfInputFile) throws IOException{
         try(DataInputStream dataInputStream = new DataInputStream(new FileInputStream(nameOfInputFile))){
-            byte[] allBytes = dataInputStream.readAllBytes();
-            int quantityOfFile = allBytes[0];
+            int quantityOfFile = dataInputStream.readInt();
             for(int z = 1; z < quantityOfFile; z++) {
-                int lengthNameOfFile = allBytes[z];
+                int lengthNameOfFile = dataInputStream.readInt();
                 System.out.println(lengthNameOfFile);
                 String nameOfDecompress = "";
                 int k;
                 for (k = 1; k <= lengthNameOfFile; k++) {
-                    char ch = (char) allBytes[k];
+                    char ch = dataInputStream.readChar();
                     nameOfDecompress += Character.toString(ch);
                 }
                 System.out.println(nameOfDecompress);
+                int lengthOfFile = dataInputStream.readInt();
                 try (DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(nameOfDecompress))) {
-                    for (int i = k; i < allBytes.length - 1; i += 2) {
-                        if(k+2<allBytes.length && allBytes[k] == 0 && allBytes[k+1]==0 && allBytes[k+2]==0){
-                            z = k+2;
-                            break;
-                        }
-                        int value = allBytes[i];
+                    for (int i = k; i < lengthOfFile - 1; i += 2) {
+                        int value = dataInputStream.readInt();
                         for (int j = 0; j < value; j++) {
-                            dataOutputStream.write(allBytes[i + 1]);
+                            dataOutputStream.write(dataInputStream.readByte());
                         }
                     }
                 }
             }
         }
+    }
+
+    byte[] byteArray(byte[] compress, int point){
+        byte[] bytes = new byte[point+1];
+        for(int i = 0; i <= point; i++){
+            bytes[i] = compress[i];
+        }
+        return compress;
     }
 }
--- src/Start.java
+++ src/Start.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Start {
     public static void main(String args[]) throws IOException {
         Compression compression1 = new Compression();
-        if(args.length>1){
+        if(args.length<1){
             compression1.decompressRLE(args[0]);
         } else {
             compression1.compressRlE(args);
